b0d825cabc4daba3f653ec9f89566928b4efb4b0
gangsterless <43644055+gangsterless@users.noreply.github.com>
2019-11-22 11:45:44 +0800

    Add files via upload
    MVC framework

---
+++ MVC/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class Model
+{
+    //名字标识
+    public abstract string Name { get; }
+
+    //发送事件
+    protected void SendEvent(string eventName,object data = null)
+    {
+        MVC.SendEvent(eventName,data);
+    }
+}
---
+++ MVC/View.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class View : MonoBehaviour {
+    //名字标识
+    public abstract string Name { get; }
+
+    //事件关心列表
+    [HideInInspector]
+    public List<string> AttentionList = new List<string>();
+
+    public virtual  void RegisterAttentionEvent() {
+
+    }
+
+    //处理事件
+    public abstract void HandleEvent(string name, object data);
+
+    //发送事件
+    protected void SendEvent(string eventName, object data = null)
+    {
+        MVC.SendEvent(eventName, data);
+    }
+
+    //获取模型
+    protected T GetModel<T>()
+        where T:Model
+    {
+        return MVC.GetModel<T>() as T;
+    }
+}
